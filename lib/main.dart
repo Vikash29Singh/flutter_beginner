24a9e596070a06a9bc3efcfae6292481f59eb60e
Vikash SIngh <vikashlikes18@gmail.com>
2020-09-04 10:09:29 +0530

    New
    Navigate from one screen to another screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,73 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Navigate to Another Activity Screen',
+    home: HomeScreen(),
+  ));
+}
+
+class HomeScreen extends StatelessWidget {
+
+  gotoSecondActivity(BuildContext context){
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => SecondScreen()),
+    );//route to next screen
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Activity Screen'),
+      ),
+      body: Center(
+
+        child: RaisedButton(
+            child: Text('Navigate To Second Screen'),
+            color: Colors.green,
+            textColor: Colors.white,
+            onPressed: () {
+              gotoSecondActivity(context);
+            }),
+
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+
+  goBackToPreviousScreen(BuildContext context){
+
+    Navigator.pop(context);
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Activity Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {goBackToPreviousScreen(context);},
+          color: Colors.lightBlue,
+          textColor: Colors.white,
+          child: Text('Go Back To Previous Screen'),
+        ),
+      ),
+    );
+  }
+}
+
+/*
 void main()
 {
   runApp(myapp());
@@ -25,3 +93,4 @@ class myapp extends StatelessWidget {
     );
   }
 }
+*/
